36ef029624168210d4ef3317901206e5b502269b
Tyler Duong <duongt@protonmail.com>
2023-06-23 03:09:17 -0700

    I love CORS

--- backend/app/api.py
+++ backend/app/api.py
@@ -13,8 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["https://api.image-search.demo.starpoint.ai", "https://unsplash-demo-backend-gjeoowx5va-uc.a.run.app"],
+    allow_credentials=False,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
